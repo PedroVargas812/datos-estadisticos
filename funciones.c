0ea7bddefb72a0bf8a77a5bebc56fd461e1174f2
PedroVargas812 <pedro.vargas@udla.edu.ec>
2024-11-14 17:12:17 -0500

    funciones.c

---
+++ funciones.c
@@ -0,0 +1,45 @@
+#include "funciones.h"
+
+void ordenarArreglo(float numeros[], float numordenados[], 
+                    float *min, float *max, float *mediana) {
+    // Copiamos los elementos del arreglo original a numordenados
+    for (int i = 0; i < 10; i++) {
+        numordenados[i] = numeros[i];
+    }
+
+    // Ordenamos el arreglo numordenados usando el algoritmo de burbuja
+    for (int i = 0; i < 10 - 1; i++) {
+        for (int j = 0; j < 10 - i - 1; j++) {
+            if (numordenados[j] > numordenados[j + 1]) {
+                // Intercambiamos los valores si están en el orden incorrecto
+                float temp = numordenados[j];
+                numordenados[j] = numordenados[j + 1];
+                numordenados[j + 1] = temp;
+            }
+        }
+    }
+    *min=numordenados[0];
+    *max=numordenados[9];
+    *mediana=numordenados[5];
+}
+
+void mediaymoda(float numeros[], float *media, int *moda){
+    float suma=0;
+    int c=0, m=0;
+    for (int i = 0; i < 10; i++)
+    {
+        suma=suma+numeros[i];
+        for (int j = 0; j < 10; j++)
+        {
+            if(numeros[i]==numeros[j]){
+                c++;
+            }
+        }
+        if(c>m){
+            m=c;
+        }
+        c=0;
+    }
+    *media=suma/10;
+    *moda=m;
+}
